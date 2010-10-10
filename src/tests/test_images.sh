57a50d7233f4be93e2e4f8d136fdf85e3d93dfa3
Dieter Plaetinck <dieter@plaetinck.be>
2010-10-10 14:18:01 +0200

    make messages more clear

--- src/tests/test_images.sh
+++ src/tests/test_images.sh
@@ -6,22 +6,23 @@ do
 	echo "#size scratch_d scratch_s scratch_sc noop_d noop_s noop_sc append_1_d append_1_s append_1_sc prepend_1_d prepend_1_s prepend_1_sc" > /tmp/rsyncbench_results_test_${type}_images.txt
 	for size in 1 100 1024
 	do
-		echo "Preparing test.."
+		echo "**** Testing $type image syncing, base size $size MiB"
+		echo "** Preparing test.."
 		line="$size "
 		rm -rf $src/*
 		rsync -a --delete $src/ $dst/
-		echo "Testing sync from scratch.."
+		echo "** Testing sync from scratch.."
 		cp $pool/data-$type-$size $src
 		measure_rsync
 		line="$line $duration $kiB_sent $kiB_sent_comp"
-		echo "Testing 'no-op' sync.."
+		echo "** Testing 'no-op' sync.."
 		measure_rsync
 		line="$line $duration $kiB_sent $kiB_sent_comp"
-		echo "Testing sync with appended $type data block"
+		echo "** Testing sync with appended $type data block"
 		cat $pool/data-$type-1 >> $src/data-$type-$size
 		measure_rsync
 		line="$line $duration $kiB_sent $kiB_sent_comp"
-		echo "Testing sync with prepended $type data block" # could be more efficient..
+		echo "** Testing sync with prepended $type data block" # could be more efficient..
 		rsync $pool/data-$type-$size $src/data-$type-$size
 		rsync -a $src/ $dst/
 		mv $src/data-$type-$size $src/data-$type-$size.tmp
@@ -35,6 +36,7 @@ do
 		# replace data block in the middle
 		# shuffle blocks in the file around
 		# replace blocks in begin, middle, end
+		"** Writing results.."
 		echo "$line">> /tmp/rsyncbench_results_test_${type}_images.txt
 	done
 done
