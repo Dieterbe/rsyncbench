402e3d91a1d06e1eefc156552c7b193a1ae7f59f
Dieter Plaetinck <dieter@plaetinck.be>
2010-10-10 11:14:54 +0200

    fix bugs with incorrect src and dst variables, that caused /* to be deleted, oopsie

--- src/tests/test_random_images.sh
+++ src/tests/test_random_images.sh
@@ -7,24 +7,24 @@ function test_run () {
 		line="$size "
 		rm -rf $src/*
 		rsync -a $src/ $dst/
-		cp $pool/data-random-$size $rsync_src
+		cp $pool/data-random-$size $src
 		# from scratch
 		measure_rsync
 		line="$line $duration $kiB_sent $kiB_sent_comp"
 		# no-op
 		measure_rsync
 		line="$line $duration $kiB_sent $kiB_sent_comp"
-		# append data block
-		cat $pool/data-random-1 >> $rsync_src/data-random-$size
+		# append random data block
+		cat $pool/data-random-1 >> $src/data-random-$size
 		measure_rsync
 		line="$line $duration $kiB_sent $kiB_sent_comp"
-		# prepend data block (this can be more efficient)
-		cp $pool/data-random-$size $rsync_src
+		# prepend random data block (this can be more efficient)
+		cp $pool/data-random-$size $src
 		rsync -a $src/ $dst/
-		mv $rsync_src/data-random-$size $rsync_src/data-random-$size.tmp
-		cp $pool/data-random-1 $rsync_src/data-random-$size
-		cat $rsync_src/data-random-$size.tmp >> $rsync_src/data-random-$size
-		rm $rsync_src/data-random-$size.tmp
+		mv $src/data-random-$size $src/data-random-$size.tmp
+		cp $pool/data-random-1 $src/data-random-$size
+		cat $src/data-random-$size.tmp >> $src/data-random-$size
+		rm $src/data-random-$size.tmp
 		measure_rsync
 		line="$line $duration $kiB_sent $kiB_sent_comp"
 		# maybe later:
